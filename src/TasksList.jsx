0a5aef6ed3ff97f8d0e71a50ff7c893a5cd6ccb4
Aizhan <104029555+AizhanSmekenova@users.noreply.github.com>
2022-06-27 01:14:24 +0600

    Add files via upload

---
+++ src/TasksList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Task from './Task';
+
+const TasksList = ({ tasks, handleTaskStatusChange, handleTaskDelete }) => {
+    const sortedList = tasks.slice().sort((a, b) => a.done - b.done);
+    return (
+        <ul className="list">
+            {sortedList.map(task => (
+                <Task
+                    key={task.id}
+                    {...task}
+                    onChange={handleTaskStatusChange}
+                    onDelete={handleTaskDelete}
+                />
+            ))}
+        </ul>
+    );
+};
+
+TasksList.propTypes = {
+    tasks: PropTypes.arrayOf(
+        PropTypes.shape({
+            text: PropTypes.string,
+            done: PropTypes.bool,
+            id: PropTypes.string,
+        }),
+    ).isRequired,
+    handleTaskStatusChange: PropTypes.func.isRequired,
+    handleTaskDelete: PropTypes.func.isRequired,
+};
+
+export default TasksList;
---
+++ src/TodoList.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import CreateTaskInput from './CreateTaskInput';
+import TasksList from './TasksList';
+import {
+    createTask,
+    updateTask,
+    deleteTask,
+    getTasksList,
+} from './tasksGateway';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+        };
+    }
+
+    componentDidMount() {
+        this.fetchTasksList();
+    }
+
+    fetchTasksList = () => {
+        getTasksList().then(tasksList => {
+            this.setState({ tasks: tasksList });
+        });
+    };
+
+    handleTaskStatusChange = id => {
+        const { tasks } = this.state;
+        const { done, text, createDate } = tasks.find(task => task.id === id);
+        const updatedTask = {
+            text,
+            createDate,
+            done: !done,
+        };
+        updateTask(id, updatedTask).then(this.fetchTasksList);
+    };
+
+    handleTaskDelete = id => {
+        deleteTask(id).then(this.fetchTasksList);
+    };
+
+    handleTaskCreate = text => {
+        createTask({
+            text,
+            done: false,
+            createDate: new Date().toISOString(),
+        }).then(this.fetchTasksList);
+    };
+
+    render() {
+        return (
+            <>
+                <h1 className="title">Todo List</h1>
+                <main className="todo-list">
+                    <CreateTaskInput onCreate={this.handleTaskCreate} />
+                    <TasksList
+                        tasks={this.state.tasks}
+                        handleTaskStatusChange={this.handleTaskStatusChange}
+                        handleTaskDelete={this.handleTaskDelete}
+                    />
+                </main>
+            </>
+        );
+    }
+}
+
+export default TodoList;
